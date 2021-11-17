bc9bcbf867f67029bd883ca03b42dadaf5711974
Ron Johnson <ronjohn4@gmail.com>
2021-11-17 10:18:07 -0700

    Docker with Compose test

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ def route_bye():
     return "see ya"
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
